033cdbe3ad8f737dd245b92e99f848574ce0c47d
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-08-02 16:58:00 -0300

    Updates + RB import preview

--- es6/rb/components/index.d.ts
+++ es6/rb/components/index.d.ts
@@ -2,3 +2,5 @@ import { FunctionalComponent, h } from 'preact';
 export declare const Dashboard: FunctionalComponent;
 export declare const RB: () => h.JSX.Element;
 export declare const ManageOrders: () => h.JSX.Element;
+export declare const SyncProducts: () => h.JSX.Element;
+export declare const PreImportPreview: () => h.JSX.Element;
--- src/rb/components/index.tsx
+++ src/rb/components/index.tsx
@@ -1,95 +1,29 @@
 import { FunctionalComponent, h, Fragment } from 'preact'
 import { rb, wc } from 'gsg-integrations'
-import { useCallback, useEffect, useState } from 'preact/hooks'
-import { Button, Stack, Heading, Tr, Td, useBoolean, VStack, Thead, Checkbox } from '@chakra-ui/react'
+import { useCallback, useMemo, useState } from 'preact/hooks'
+import { Button, Stack, Heading, Tr, Td, useBoolean, VStack, Thead, Checkbox, Box, chakra, HStack, SimpleGrid } from '@chakra-ui/react'
 import * as hooks from '../../hooks'
 import { useArray, usePromiseCall } from '../../hooks'
 import { useOptionsContext, OptionInput } from '../../hooks/options'
 import { Post } from '../../wp'
+import { CheckboxIndexItem, useContext as useCheckboxIndexContext } from '../../components/CheckboxIndex'
 import { SimpleAccordion, SimplePanel } from '../../components/SimpleAccordion'
-import { SimpleTable } from '../../components/SimpleTable'
 import RadioOptions from '../../components/RadioOptions'
-import { useWC, PaginatedActionsCheckListTable } from '../../wc'
+import { useWC, PaginatedActionsCheckListTable, Product } from '../../wc'
 import { useOrder } from '../../wc/order'
 import { useRB } from '../../hooks/rb'
 import { useAN } from '../../hooks/an'
+import { useProduct } from 'src/wc/product'
+import { chunk } from '@common'
 
 export const Dashboard: FunctionalComponent = () => {
-	const { client: wcC } = useWC()
-	const { client: rbC } = useRB()
-	const { client: anC } = useAN()
-	const depts = usePromiseCall(useCallback(rbC.getDepartments, [rbC]), [rbC])
-	const [dept, setDept] = useState<string | null>(null)
-	const cats = usePromiseCall(
-		useCallback(dept ? rbC.getCategories.bind(null, dept) : () => Promise.reject('No dept selected'), [dept]),
-		[dept]
-	)
-	const [cat, setCat] = useState<string | null>(null)
-	const created = useArray<wc.Product.Type>([])
-	const updated = useArray<wc.Product.Type>([])
-	const [syncing, setSyncing] = useBoolean(false)
-	const syncProducts = useCallback(() => {
-		if (!dept || !cat) {
-			return
-		}
-		setSyncing.on()
-		rbC.getProducts(dept, cat)
-			.then(rb.syncProductsWithWooCommerce(wcC))
-			.then(promises => {
-				return promises.map(promise => {
-					return promise.then(res => {
-						if (res.create) created.concat(res.create)
-						if (res.update) updated.concat(res.update)
-					})
-				})
-			})
-			.finally(setSyncing.off)
-	}, [dept, cat])
 
 	return (
 		<VStack>
 			<Heading size='lg'>RB Integration Dashboard</Heading>
 			<SimpleAccordion>
 				<SimplePanel title='Sync Products'>
-					<VStack w='100%' alignItems='stretch'>
-						{depts.resolved ? <RadioOptions onChange={setDept} options={depts.resolved} /> : 'Loading Deparments'}
-						{cats.resolved ? <RadioOptions onChange={setCat} options={cats.resolved} /> : 'Loading Categories'}
-						<Button onClick={syncProducts} disabled={!dept || !cat || syncing}>
-							Sync Products
-						</Button>
-						{created.array.length > 0 || updated.array.length > 0 ? (
-							<SimpleTable headers={['ID#', 'Name', 'SKU', 'Regular Price & Sales Price', 'Storage Quantity']}>
-								<Thead>Created {created.array.length}</Thead>
-								{created.array.map(p => (
-									<Tr>
-										<Td>{p.id}</Td>
-										<Td>{p.name}</Td>
-										<Td>{p.sku}</Td>
-										<Td>
-											{p.regular_price}
-											<br />
-											{p.sale_price}
-										</Td>
-										<Td>{p.stock_quantity}</Td>
-									</Tr>
-								))}
-								<Thead>Updated {updated.array.length}</Thead>
-								{updated.array.map(p => (
-									<Tr>
-										<Td>{p.id}</Td>
-										<Td>{p.name}</Td>
-										<Td>{p.sku}</Td>
-										<Td>
-											{p.regular_price}
-											<br />
-											{p.sale_price}
-										</Td>
-										<Td>{p.stock_quantity}</Td>
-									</Tr>
-								))}
-							</SimpleTable>
-						) : null}
-					</VStack>
+					<SyncProducts/>
 				</SimplePanel>
 				<SimplePanel title='Post Orders'>
 					<ManageOrders/>
@@ -130,7 +64,6 @@ export const RB = () => {
 }
 
 export const ManageOrders = () => {
-	const { options } = hooks.useOptions()
 	const Order = useOrder()
 	const WC = useWC()
 	const RB = useRB()
@@ -178,4 +111,102 @@ export const ManageOrders = () => {
 			}}
 		</PaginatedActionsCheckListTable>
 	)
+}
+
+export const SyncProducts = () => {
+	const { client: wcC } = useWC()
+	const { client: rbC } = useRB()
+	const depts = usePromiseCall(useCallback(rbC.getDepartments, [rbC]), [rbC])
+	const [dept, setDept] = useState<string | null>(null)
+	const cats = usePromiseCall(
+		useCallback(dept ? rbC.getCategories.bind(null, dept) : () => Promise.reject('No dept selected'), [dept]),
+		[dept]
+	)
+	const [cat, setCat] = useState<string | null>(null)
+	const created = useArray<Partial<wc.Product.Type>>([])
+	const updated = useArray<Partial<wc.Product.Type>>([])
+	const [syncing, setSyncing] = useBoolean(false)
+	const syncProducts = useCallback(() => {
+		if (!dept || !cat) {
+			return
+		}
+		setSyncing.on()
+		rbC.getProducts(dept, cat)
+			.then(rb.syncProductsWithWooCommerce(wcC))
+			.then((res) => {
+				if (res.create) created.concat(res.create)
+				if (res.update) updated.concat(res.update)
+			})
+			.finally(setSyncing.off)
+	}, [dept, cat])
+
+	type Index = Record<string, Partial<wc.Product.Type>>
+
+	const indexes = useMemo( () => {
+		return chunk([...created.array, ...updated.array], 100).map( arr => (
+			arr.reduce<Index>( (acc, p) => {
+				// @ts-expect-error
+				acc[p.sku] = p
+				return acc
+			}, {} as Index )
+		) )
+	}, [created, updated] )
+
+	return (
+		<VStack w='100%'>
+			<VStack w='100%' alignItems='stretch'>
+				{depts.resolved ? <RadioOptions onChange={setDept} options={depts.resolved} /> : 'Loading Deparments'}
+				{cats.resolved ? <RadioOptions onChange={setCat} options={cats.resolved} /> : 'Loading Categories'}
+				<Button onClick={syncProducts} disabled={!dept || !cat || syncing}>
+					Search Products
+				</Button>
+			</VStack>
+			{indexes.length > 0 ? (
+				<SimpleAccordion>
+					{indexes?.map( index => (
+						<Product.PreImport index={index}>
+							<PreImportPreview/>
+						</Product.PreImport>
+					) )}
+				</SimpleAccordion>
+			) : null}
+		</VStack>
+	)
+}
+
+export const PreImportPreview = () => {
+	const { index } = useCheckboxIndexContext()
+	const Product = useProduct()
+	const findBySku = useCallback(
+		( s : string ) => Product.array.find( ({sku}) => sku === s ),
+		[Product.array],
+	)
+	return (<SimpleGrid columns={2} gap={2} >{( Object.keys( index ).map( (sku) => {
+		const product = findBySku(sku) ?? (index[sku] as wc.Product.Type)
+		const id = product.id
+		return (
+			<chakra.label p={4} w='100%'bg={id ? 'blue.200' : 'green.200'} justifyContent='flex-start' >
+				<HStack>
+					<CheckboxIndexItem id={sku} />
+					<Heading w='100%' size='sm' >{
+						id ? 'Update' : 'Create'
+					}</Heading>
+				</HStack>
+				<HStack>
+					<VStack w='100%' alignItems='flex-start' >
+						{product.id ?(<Box>ID: {product.id}</Box>) : null}
+						<Box>Name: {product.name}</Box>
+						<Box>Sku: {product.sku}</Box>
+						<Box>Categories: {product.categories?.map( c => c.name ).join(', ')}</Box>
+						<Box>Regular price: {product.regular_price}</Box>
+						<Box>Sale price: {product.sale_price}</Box>
+						<Box>Stock quantity: {product.stock_quantity}</Box>
+						<Box>Description: {product.description}</Box>
+						<Box>On sale start date: {product.date_on_sale_from ? (new Date(product.date_on_sale_from).toLocaleString()) : null}</Box>
+						<Box>On sale end date: {product.date_on_sale_to ? (new Date(product.date_on_sale_to).toLocaleString()) : null}</Box>
+					</VStack>
+				</HStack>
+			</chakra.label>
+		)
+	} ) )}</SimpleGrid>)
 }
